58a5c5e402fd96bd4618d901a9a151ef577b8260
youngsik823 <youngsik823@naver.com>
2024-09-13 23:59:39 +0900

    fix(메인페이지) : 아동용 티켓오픈 레이아웃 및 기능 구현 (#27)

---
+++ src/components/Mainpage/kidTicketOpen/KidTicketOpen.styled.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+export const KidTicketOpenContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const KidTicketOpenWrap = styled.div`
+  width: 1120px;
+  height: 331px;
+`;
+
+export const KidTicketOpenHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & > a {
+    display: inline-block;
+    color: #242428;
+    font-size: 15px;
+  }
+
+  & > h1 {
+    font-weight: 500;
+    font-size: 32px;
+    color: #242428;
+  }
+`;
+
+export const KidTicketOpenBox = styled.div`
+  display: flex;
+  margin-top: 30px;
+  justify-content: space-between;
+`;
+
+export const KidTicketOpenItem = styled.div`
+  width: 200px;
+  cursor: pointer;
+`;
+
+export const KidTicketOpenImgBox = styled.div`
+  width: inherit;
+  height: 247px;
+  border-radius: 10px;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    transition: transform 0.3s ease;
+  }
+
+  &:hover > img {
+    height: 247px;
+    transform: scale(1.1);
+  }
+`;
+
+export const KidTicketOpenTitle = styled.p`
+  margin-top: 10px;
+  font-size: 18px;
+`;
+
+export const KidTicketOpenDate = styled.p`
+  margin-top: 10px;
+  font-size: 14px;
+  color: #fa2828;
+`;
--- src/pages/Mainpage/Mainpage.jsx
+++ src/pages/Mainpage/Mainpage.jsx
@@ -3,6 +3,7 @@ import { useDetail } from '../../hooks/temp';
 import Banner from '../../components/Mainpage/banner/Banner';
 import TicketOpen from '../../components/Mainpage/ticketOpen/TicketOpen';
 import GenreAndAreaRank from '../../components/Mainpage/genreAndAreaRank/GenreAndAreaRank';
+import KidTicketOpen from '../../components/Mainpage/kidTicketOpen/KidTicketOpen';
 
 export default function Mainpage() {
   const { data } = useDetail();
@@ -15,6 +16,7 @@ export default function Mainpage() {
       <Banner />
       <TicketOpen />
       <GenreAndAreaRank />
+      <KidTicketOpen />
     </div>
   );
 }
