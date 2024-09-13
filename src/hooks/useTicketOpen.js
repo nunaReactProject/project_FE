a27f94e3575bf5fd4f0b076bb841ae3cbd3dc493
youngsik823 <youngsik823@naver.com>
2024-09-14 00:01:02 +0900

    fix(메인페이지) : 이중호출 제거 (#27)

--- src/hooks/useTicketOpen.js
+++ src/hooks/useTicketOpen.js
@@ -13,61 +13,27 @@ const fetchTicketOpen = async () => {
   const currentDate = new Date();
   const yesterdayDate = new Date();
   yesterdayDate.setDate(currentDate.getDate() - 1);
-
-  const formattedCurrentDate = getFormattedDate(currentDate);
   const formattedYesterdayDate = getFormattedDate(yesterdayDate);
 
-  // 첫 번째 API 요청
   let response = await productOpenApi({
     method: 'get',
     url: '/pblprfr',
     params: {
-      stdate: formattedCurrentDate,
       cpage: 1,
       rows: 5,
       prfstate: '02'
     }
   });
-
   const parser = new XMLParser();
-  let jsonData = parser.parse(response.data);
-
-  // dbs의 개수가 5 미만일 때 두 번째 요청
-  if (jsonData.dbs.db.length < 5) {
-    response = await productOpenApi({
-      method: 'get',
-      url: '/pblprfr',
-      params: {
-        stdate: formattedYesterdayDate,
-        cpage: 1,
-        rows: 5,
-        prfstate: '02'
-      }
-    });
-
-    jsonData = parser.parse(response.data);
-  }
-
-  const mt20ids = jsonData.dbs.db.map((item) => item.mt20id).filter((id) => id);
-
-  const fetchPerformanceDetails = async (id) => {
-    const response = await productOpenApi({
-      method: 'get',
-      url: `/pblprfr/${id}`
-    });
-    const xmlData = response.data;
-    const jsonData = parser.parse(xmlData);
-    return jsonData;
-  };
-  const performanceDetails = await Promise.all(mt20ids.map(fetchPerformanceDetails));
-
-  return performanceDetails;
+  const jsonData = parser.parse(response.data);
+  return jsonData;
 };
 
 export const useTicketOpenQuery = () => {
   return useQuery({
     queryKey: ['ticket-open'],
     queryFn: fetchTicketOpen,
+    refetchOnWindowFocus: false,
     select: (result) => result
   });
 };
