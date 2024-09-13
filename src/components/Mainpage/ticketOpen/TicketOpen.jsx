6ac9a9ad7a02a2e82c84b718341b98e3810807e3
youngsik823 <youngsik823@naver.com>
2024-09-14 00:00:16 +0900

    fix(메인페이지) : 날짜 데이터 구축 (#27)

--- src/components/Mainpage/ticketOpen/TicketOpen.jsx
+++ src/components/Mainpage/ticketOpen/TicketOpen.jsx
@@ -4,6 +4,7 @@ import { useTicketOpenQuery } from '../../../hooks/useTicketOpen';
 
 const TicketOpen = () => {
   const { data } = useTicketOpenQuery();
+  const date = ['09.19(목) 18:00', '09.20(금) 20:00', '09.23(월) 17:00', '09.23(월) 19:00', '09.23(월) 20:00'];
 
   return (
     <to.TicketOpenContainer>
@@ -13,13 +14,13 @@ const TicketOpen = () => {
           <a href='#'>전체보기</a>
         </to.TicketOpenHeader>
         <to.TicketOpenBox>
-          {data?.map((item, index) => (
+          {data?.dbs.db.map((item, index) => (
             <to.TicketOpenItem key={index}>
               <to.TicketOpenImgBox>
-                <img src={item.dbs.db.poster} alt={item.dbs.db.prfnm} />
+                <img src={item.poster} alt={item.prfnm} />
               </to.TicketOpenImgBox>
-              <to.TicketOpenTitle>{item.dbs.db.prfnm}</to.TicketOpenTitle>
-              <to.TicketOpenDate>{item.dbs.db.dtguidance}</to.TicketOpenDate>
+              <to.TicketOpenTitle>{item.prfnm}</to.TicketOpenTitle>
+              <to.TicketOpenDate>{date[index]}</to.TicketOpenDate>
             </to.TicketOpenItem>
           ))}
         </to.TicketOpenBox>
