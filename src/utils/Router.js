6af249f273bd7aba78c6d7b60aae6de305b8f3f4
jeong-minG <ika818488@gmail.com>
2024-09-14 19:48:13 +0900

    feat/search

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -4,6 +4,7 @@ import Mainpage from './../pages/Mainpage/Mainpage';
 import Header from '../components/common/Header/Header';
 import Footer from '../components/common/Footer/Footer';
 import Login from '../pages/Login/Login';
+import SearchPage from '../pages/SearchPage/SearchPage';
 
 export default function Router() {
   return (
@@ -12,6 +13,7 @@ export default function Router() {
       <Routes>
         <Route path='/' element={<Mainpage />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/search' element={<SearchPage />} />
       </Routes>
       <Footer />
     </>
