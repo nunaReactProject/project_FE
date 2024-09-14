64631a7a3e1657dbaf525891c40c8edbddc5f74f
Byeong Min Cho <merrybmc@gmail.com>
2024-09-14 21:20:40 +0900

    fix : loginlayout (#49)

--- src/components/Login/PasswordInput.styled.js
+++ src/components/Login/PasswordInput.styled.js
@@ -1,33 +1,45 @@
 import styled from 'styled-components';
 import { CiLock } from 'react-icons/ci';
 
-export const StyledBox = styled.div`
+export const InputContainer = styled.div`
   display: flex;
-  align-items: center;
-  width: 400px;
-  border-left-width: 1px;
-  border-right-width: 1px;
-  border-bottom-width: 1px;
-  border-top-width: 0px;
-  border-style: solid;
-  border-color: #dddddd;
-  padding: 15px;
-  border-bottom-right-radius: 10px;
-  border-bottom-left-radius: 10px;
+  flex-direction: column;
+  gap: 10px;
+  width: 430px;
+  border-bottom: 1px solid black;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+  margin-top: 10px;
 `;
 
-export const UserIcon = styled(CiLock)``;
+export const InputBox = styled.div`
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  padding-left: 10px;
+`;
+
+export const Title = styled.div`
+  width: 100px;
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 30px;
+  padding-left: 4px;
+`;
 
 export const Input = styled.input`
-  width: 300px;
-  font-size: 15px;
-  margin-left: 14px;
-  height: 24px;
+  width: 380px;
   border: none;
   outline: none;
+  font-size: 15px;
+  line-height: 30px;
 
   &::placeholder {
-    font-size: 15px;
-    color: #b399a6;
+    color: #cccccf;
   }
+`;
+
+export const UserIcon = styled(CiLock)`
+  color: #b399a6;
 `;
