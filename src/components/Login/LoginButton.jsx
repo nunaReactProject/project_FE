bc4ebd24c34df6d3718470b558ca9e72e5a8de2f
Byeong Min Cho <merrybmc@gmail.com>
2024-09-15 17:08:08 +0900

    feat : login valid (#102)

--- src/components/Login/LoginButton.jsx
+++ src/components/Login/LoginButton.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import * as S from './LoginButton.styled';
 
-export default function LoginButton({ onLogin }) {
-  return <S.Button onClick={onLogin}>로그인</S.Button>;
+export default function LoginButton({ onLogin, warning, buttonState }) {
+  return (
+    <>
+      {warning && <S.Warning>{warning}</S.Warning>}
+      <S.Button onClick={onLogin} buttonState={buttonState}>
+        로그인
+      </S.Button>
+    </>
+  );
 }
