acb867dbc293bad067201b91215f0a1e80cf66e2
Byeong Min Cho <merrybmc@gmail.com>
2024-09-15 12:48:59 +0900

    feat : login api (#82)

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from '../../components/Login/TItle';
 import * as S from './Login.styled';
 import IdInput from '../../components/Login/IdInput';
 import PasswordInput from '../../components/Login/PasswordInput';
 import LoginButton from '../../components/Login/LoginButton';
 import SocialLogin from '../../components/Login/SocialLogin';
+import { useLoginMutation } from '../../hooks/useLoginMutation';
 
 export default function Login() {
+  const [info, setInfo] = useState({ userId: '', password: '' });
+
+  const { mutate: loginMutate } = useLoginMutation();
+
+  const onLogin = () => {
+    loginMutate({ info });
+  };
+
   return (
     <S.Container>
       <Title />
-      <IdInput />
-      <PasswordInput />
-      <LoginButton />
+      <IdInput info={info} setInfo={setInfo} />
+      <PasswordInput info={info} setInfo={setInfo} />
+      <LoginButton onLogin={onLogin} />
       <SocialLogin />
     </S.Container>
   );
