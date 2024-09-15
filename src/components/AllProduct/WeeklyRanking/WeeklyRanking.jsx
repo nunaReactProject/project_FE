ff1fca3b4b5dcdcb6505bc7c734bac5a966b7e96
etesongg <etesong0114@gmail.com>
2024-09-15 15:42:32 +0900

    fix: weekly ranking background color 화이트로 수정

--- src/components/AllProduct/WeeklyRanking/WeeklyRanking.jsx
+++ src/components/AllProduct/WeeklyRanking/WeeklyRanking.jsx
@@ -7,9 +7,6 @@ import { Spinner } from '@chakra-ui/react';
 
 const WeeklyRanking = ({ ststype, date }) => {
   const { data, isLoading, error } = useWeeklyRankProductQuery({ ststype, date });
-  const canvasRef = useRef(null);
-  const [bgColor, setBgColor] = useState('');
-  const [colorCache, setColorCache] = useState({});
   const [itemsToShow, setItemsToShow] = useState(5);
   const navigate = useNavigate();
 
@@ -31,61 +28,13 @@ const WeeklyRanking = ({ ststype, date }) => {
     };
   }, []);
 
-  useEffect(() => {}, [data]);
-
-  const getAverageColor = (imageUrl) => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    const img = new Image();
-
-    img.crossOrigin = 'Anonymous';
-    img.src = imageUrl;
-
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      const data = imageData.data;
-      let r = 0,
-        g = 0,
-        b = 0;
-
-      for (let i = 0; i < data.length; i += 4) {
-        r += data[i];
-        g += data[i + 1];
-        b += data[i + 2];
-      }
-
-      const pixelCount = data.length / 4;
-      r = Math.floor(r / pixelCount);
-      g = Math.floor(g / pixelCount);
-      b = Math.floor(b / pixelCount);
-
-      setBgColor(`rgb(${r}, ${g}, ${b})`);
-      setColorCache((prev) => ({ ...prev, [imageUrl]: `rgb(${r}, ${g}, ${b})` }));
-    };
-
-    img.onerror = (err) => {
-      console.error('Error loading image:', err);
-    };
-  };
-
-  const handleMouseEnter = (imageUrl) => {
-    const fullImageUrl = `https://cors-anywhere.herokuapp.com/http://www.kopis.or.kr${imageUrl}`;
-    if (colorCache[fullImageUrl]) {
-      setBgColor(colorCache[fullImageUrl]);
-    } else {
-      getAverageColor(fullImageUrl);
-    }
-  };
-
   const goToDetailPage = (id) => {
     navigate(`/detail/${id}`);
   };
 
   useEffect(() => {
     if (data && data.length > 0) {
-      const firstProductImageUrl = `https://cors-anywhere.herokuapp.com/http://www.kopis.or.kr${data[0].poster}`;
-      getAverageColor(firstProductImageUrl);
+      const firstProductImageUrl = `http://www.kopis.or.kr${data[0].poster}`;
     }
   }, [data]);
 
@@ -104,25 +53,18 @@ const WeeklyRanking = ({ ststype, date }) => {
   const products = data?.slice(0, itemsToShow);
 
   return (
-    <div>
-      <S.WeeklyRankingSection>
-        <S.WeeklyRankingBg style={{ backgroundColor: bgColor }}></S.WeeklyRankingBg>
-        <S.HeaderRanking>랭킹</S.HeaderRanking>
-        <S.SubHeaderRanking>주간베스트</S.SubHeaderRanking>
-        <S.ProductList>
-          {products.map((product) => (
-            <div
-              key={product.id}
-              onMouseEnter={() => handleMouseEnter(product.poster)}
-              onClick={() => goToDetailPage(product.mt20id)}
-              style={{ cursor: 'pointer' }}>
-              <WeeklyRankingCard products={[product]} />
-            </div>
-          ))}
-        </S.ProductList>
-      </S.WeeklyRankingSection>
-      <canvas ref={canvasRef} style={{ display: 'none' }} width={1} height={1}></canvas>
-    </div>
+    <S.WeeklyRankingSection>
+      <S.WeeklyRankingBg></S.WeeklyRankingBg>
+      <S.HeaderRanking>랭킹</S.HeaderRanking>
+      <S.SubHeaderRanking>주간베스트</S.SubHeaderRanking>
+      <S.ProductList>
+        {products.map((product) => (
+          <div key={product.id} onClick={() => goToDetailPage(product.mt20id)} style={{ cursor: 'pointer' }}>
+            <WeeklyRankingCard products={[product]} />
+          </div>
+        ))}
+      </S.ProductList>
+    </S.WeeklyRankingSection>
   );
 };
 
